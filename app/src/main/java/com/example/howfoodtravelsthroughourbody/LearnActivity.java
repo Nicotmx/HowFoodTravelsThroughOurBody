6a676d8979bac5797004de2242daba91f67299e3
Nicotmx <mayxin05@gmail.com>
2026-06-11 03:58:58 +0800

    Completed splash screen, home screen and learn activity planning

--- app/src/main/java/com/example/howfoodtravelsthroughourbody/LearnActivity.java
+++ app/src/main/java/com/example/howfoodtravelsthroughourbody/LearnActivity.java
@@ -1,24 +1,83 @@
 package com.example.howfoodtravelsthroughourbody;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.media.MediaPlayer;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class LearnActivity extends AppCompatActivity {
+
+    ImageButton btnMouthAudio, btnEsophagusAudio, btnStomachAudio;
+    ImageButton btnSmallIntestineAudio, btnLargeIntestineAudio;
+    Button btnBackHome;
+    CardView btnWatchVideo;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_learn);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnMouthAudio = findViewById(R.id.btnMouthAudio);
+        btnEsophagusAudio = findViewById(R.id.btnEsophagusAudio);
+        btnStomachAudio = findViewById(R.id.btnStomachAudio);
+        btnSmallIntestineAudio = findViewById(R.id.btnSmallIntestineAudio);
+        btnLargeIntestineAudio = findViewById(R.id.btnLargeIntestineAudio);
+        btnWatchVideo = findViewById(R.id.btnWatchVideo);
+        btnBackHome = findViewById(R.id.btnBackHome);
+
+        btnMouthAudio.setOnClickListener(v -> {
+            Toast.makeText(this, "Mouth audio clicked", Toast.LENGTH_SHORT).show();
+            playAudio(R.raw.mouth_audio);
         });
+        btnEsophagusAudio.setOnClickListener(v -> playAudio(R.raw.esophagus_audio));
+        btnStomachAudio.setOnClickListener(v -> playAudio(R.raw.stomach_audio));
+        btnSmallIntestineAudio.setOnClickListener(v -> playAudio(R.raw.small_intestine_audio));
+        btnLargeIntestineAudio.setOnClickListener(v -> playAudio(R.raw.large_intestine_audio));
+
+        btnWatchVideo.setOnClickListener(v -> {
+            Intent intent = new Intent(LearnActivity.this, VideoActivity.class);
+            startActivity(intent);
+        });
+
+        btnBackHome.setOnClickListener(v -> finish());
+    }
+
+    private void playAudio(int audioResId) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        mediaPlayer = MediaPlayer.create(this, audioResId);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(mp -> {
+                mp.release();
+                mediaPlayer = null;
+            });
+        } else {
+            Toast.makeText(this, "Audio cannot play", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/howfoodtravelsthroughourbody/VideoActivity.java
@@ -0,0 +1,44 @@
+package com.example.howfoodtravelsthroughourbody;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        videoView = findViewById(R.id.videoView);
+        btnBack = findViewById(R.id.btnBack);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.digestion_video);
+        videoView.setVideoURI(videoUri);
+        videoView.requestFocus();
+
+        videoView.setOnPreparedListener(mp -> {
+            Toast.makeText(this, "Video is ready. Press play.", Toast.LENGTH_SHORT).show();
+        });
+
+        videoView.setOnErrorListener((mp, what, extra) -> {
+            Toast.makeText(this, "Video cannot play. Try another MP4 file.", Toast.LENGTH_LONG).show();
+            return true;
+        });
+
+        btnBack.setOnClickListener(v -> finish());
+    }
+}
