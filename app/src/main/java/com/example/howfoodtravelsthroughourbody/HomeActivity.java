9c84d3373b9650baf6f4159accdd22a89737a0bc
Nicotmx <mayxin05@gmail.com>
2026-06-10 21:01:53 +0800

    Created splash screen and home screen UI

---
+++ app/src/main/java/com/example/howfoodtravelsthroughourbody/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.example.howfoodtravelsthroughourbody;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    CardView btnLearn, btnActivities, btnQuiz, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLearn = findViewById(R.id.btnLearn);
+        btnActivities = findViewById(R.id.btnActivities);
+        btnQuiz = findViewById(R.id.btnQuiz);
+        btnAbout = findViewById(R.id.btnAbout);
+
+        // Learn Screen
+        btnLearn.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, LearnActivity.class);
+            startActivity(intent);
+        });
+
+        // Activities Screen (later)
+        btnActivities.setOnClickListener(v -> {
+            // TODO: Open ActivitiesMenuActivity
+        });
+
+        // Quiz Screen (later)
+        btnQuiz.setOnClickListener(v -> {
+            // TODO: Open QuizActivity
+        });
+
+        // About Screen (later)
+        btnAbout.setOnClickListener(v -> {
+            // TODO: Open AboutActivity
+        });
+    }
+}
